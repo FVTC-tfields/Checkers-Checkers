8f17cf30188a6a4973bca21a8494d584f2a01b4d
Simon Schick <300054183@fvtc.edu>
2024-02-19 14:01:55 -0600

    Added a BL Models and PL Test projects

---
+++ Checkers/Checkers.BL.Models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Checkers.BL.Models
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Checkers/Checkers.PL.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Checkers.PL.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
