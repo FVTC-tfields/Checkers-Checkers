4a47c3f6e86a202c12e3ebead6cd7677ed2932b0
Simon Schick <300054183@fvtc.edu>
2024-02-24 11:42:12 -0600

    Added the migration handler

---
+++ Checkers.MigrationHandler/Program.cs
@@ -0,0 +1,14 @@
+﻿using Checkers.PL.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContextPool<CheckersEntities>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"), builder => {
+    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+}));
+
+var app = builder.Build();
+app.Run();
+
