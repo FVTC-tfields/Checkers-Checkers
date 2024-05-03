0278377fee5d9c5412131bd59ec01232a8670772
Simon Schick <300054183@fvtc.edu>
2024-05-03 10:16:43 -0500

    trying getting movement to work

---
+++ Checkers.UI2/Models/GamePiece.cs
@@ -0,0 +1,17 @@
+﻿namespace Checkers.UI2.Models
+{
+    public class GamePiece
+    {
+        public int row { get; set; }
+        public int column { get; set; }
+        public Direction direction { get; set; }
+        public string Color { get; set; }
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Both
+    }
+}
