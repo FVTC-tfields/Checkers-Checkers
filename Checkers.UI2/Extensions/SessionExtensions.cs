d8c59441be593ceaf0983051c5f0d5551a29ef0e
Simon Schick <300054183@fvtc.edu>
2024-05-08 10:57:46 -0500

    I think I added User Authentication.

---
+++ Checkers.UI2/Extensions/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace Checkers.UI2.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+
+
+    } // End of SessionExtensions class
+}
